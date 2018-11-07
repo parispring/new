60041dd5e48ff51f1562446dcab18d57c90ebb43
shibo <parispring2008@163.com>
2018-11-08 00:12:40 +0800

    Initial commit

---
+++ src/main/java/shibo/com/App.java
@@ -0,0 +1,13 @@
+package shibo.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
