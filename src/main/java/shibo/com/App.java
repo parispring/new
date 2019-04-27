0002dd2e463341832cd4a89515f13f46010a3082
shibo <parispring2008@163.com>
2019-04-27 21:21:57 +0800

    的方法

--- src/main/java/shibo/com/App.java
+++ src/main/java/shibo/com/App.java
@@ -4,10 +4,10 @@ package shibo.com;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "我是菜鸟!" );
+        System.out.println( "我是菜鸟，哈哈!" );
     }
 }
